612a6ecbce20b75569451235ba6e24d51961c9eb
Willem Melching <willem.melching@gmail.com>
2020-10-01 16:00:52 +0200

    testing closet changes

---
+++ update_testing_closet.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash -e
+
+BRANCH=${1:-testing-closet}
+
+git fetch
+git checkout -f $BRANCH
+git reset --hard origin/$BRANCH
+git submodule update --init
+rm -f .overlay_init
+reboot || sudo reboot
