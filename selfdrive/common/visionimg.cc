cf0eb262dea4a1b3d2a7b70874d491084cbbd231
Willem Melching <willem.melching@gmail.com>
2021-07-26 14:05:47 +0200

    try EGL_LINUX_DMA_BUF_EXT

--- selfdrive/common/visionimg.cc
+++ selfdrive/common/visionimg.cc
@@ -27,7 +27,7 @@ EGLImageTexture::EGLImageTexture(const VisionBuf *buf) {
                              buf->stride/bpp, buf->len/buf->stride,
                              buf->width, buf->height);
 
-  // GraphicBuffer is ref counted by EGLClientBuffer(ANativeWindowBuffer), no need and not possible to release. 
+  // GraphicBuffer is ref counted by EGLClientBuffer(ANativeWindowBuffer), no need and not possible to release.
   GraphicBuffer* gb = new GraphicBuffer(buf->width, buf->height, (PixelFormat)format,
                                         GraphicBuffer::USAGE_HW_TEXTURE, buf->stride/bpp, (private_handle_t*)private_handle, false);
 
@@ -61,13 +61,18 @@ EGLImageTexture::EGLImageTexture(const VisionBuf *buf) {
   eglInitialize(display, NULL, NULL);
   std::cout << "egl error: " << eglGetError() << std::endl;
 
-  // TODO: can we use EGL_NATIVE_BUFFER_ANDROID instead?
-  EGLint img_attrs[] = {EGL_IMAGE_PRESERVED_KHR, EGL_TRUE,
-                        EGL_WIDTH, (EGLint)buf->width,
-                        EGL_HEIGHT, (EGLint)buf->height,
-                        EGL_NONE};
-  img_khr = eglCreateImageKHR(display, EGL_NO_CONTEXT,
-                              EGL_GL_TEXTURE_2D_KHR, buf->addr, img_attrs);
+	EGLint img_attrs[] = {
+		EGL_WIDTH, buf->width,
+		EGL_HEIGHT, buf->height,
+		EGL_LINUX_DRM_FOURCC_EXT, DRM_FORMAT_RGB888,
+		EGL_DMA_BUF_PLANE0_FD_EXT, buf->fd,
+		EGL_DMA_BUF_PLANE0_OFFSET_EXT, 0,
+		EGL_DMA_BUF_PLANE0_PITCH_EXT, buf->width * 3,
+		EGL_NONE
+	};
+
+	EGLImageKHR img_khr = eglCreateImageKHR(display, EGL_NO_CONTEXT, EGL_LINUX_DMA_BUF_EXT, 0, img_attrs);
+
   GLenum err;
   while ((err = glGetError()) != GL_NO_ERROR) {
     std::cout << "OpenGL error: " << err << std::endl;
